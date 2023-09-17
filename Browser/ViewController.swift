26bf0575c25d818470a8232c5513af7a5e9e1fd5
khlestkinaleksey <khlestkinvarshaveraleksey@gmail.com>
2023-09-17 14:42:37 +0300

    Added functions UITextFieldDelegate and UINavigationDelegate

--- Browser/ViewController.swift
+++ Browser/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        urlTextField.delegate = self
+        webView.navigationDelegate = self
+        
         let homePage = "https://apple.com"
         let url = URL(string: homePage)
         let request = URLRequest(url: url!)
@@ -28,9 +31,37 @@ class ViewController: UIViewController {
     }
 
     @IBAction func returnPressed(_ sender: UIButton) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
     }
     @IBAction func backReturn(_ sender: UIButton) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
     }
     
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let urlString = textField.text!
+        let url = URL(string: urlString)
+        let request = URLRequest(url: url!)
+        
+        webView.load(request)
+        
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        urlTextField.text = webView.url?.absoluteString
+        
+        backButton.isEnabled = webView.canGoBack
+        returnButton.isEnabled = webView.canGoForward
+    }
 }
 
